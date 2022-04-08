2915ef64672ed7d063abb606364ae192c19e8179
Bilgihan Kose <bilgihankose@gmail.com>
2022-04-08 09:06:53 +0300

    added ref, forward ref

--- src/Components/UI/Input.js
+++ src/Components/UI/Input.js
@@ -1,16 +1,19 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label className={classes.label} htmlFor={props.input.id}>
         {props.label}
       </label>
       <input
+        ref={ref}
         {...props.input} //bu neden yazılıyor not cikar
       />
     </div>
   );
-};
+});
 
 export default Input;
