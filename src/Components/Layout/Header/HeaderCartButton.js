f1f286e739d73ae3fdfca1f6a7782b5c82f605ab
Bilgihan Kose <bilgihankose@gmail.com>
2022-04-10 11:48:37 +0300

    added useEffect for header button animation

--- src/Components/Layout/Header/HeaderCartButton.js
+++ src/Components/Layout/Header/HeaderCartButton.js
@@ -1,16 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../../Cart/CartIcon";
 import CartContext from "../../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighligthed, setBtnIsHighligted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  //object destructuring
+  const { items } = cartCtx;
+
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    btnIsHighligthed ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighligted(true);
+
+    //it is best practice for calling cleanup function when you use useEffect hook
+    const timer = setTimeout(() => {
+      setBtnIsHighligted(false); //because if we don't clear this state it will work for only one time.
+    }, 300); //300 because duration of bump animation
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]); // with object destructuring we only use items for dependency
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
